b3d10bc734f785a17711009bb04652042e86a367
stvasilev <49264431+stvasilev@users.noreply.github.com>
2020-07-29 16:23:32 +0300

    set font family
    trying a font that's samey on both linux/mac/win (important for label spacing)

--- src/ipinfo/Main.java
+++ src/ipinfo/Main.java
@@ -1,6 +1,7 @@
 package ipinfo;
 
 import javafx.application.Application;
+import javafx.beans.binding.Bindings;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -11,11 +12,11 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("ipinfo.fxml"));
+        root.styleProperty().bind(Bindings.format("-fx-font-size: 10pt; -fx-font-family: Arial,Helvetica,sans-serif;"));
         primaryStage.setTitle("IP info getter");
         primaryStage.setScene(new Scene(root, 650, 600));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
